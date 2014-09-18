0e36023b739a925b71028ca0d44667bd3058123c
Gabor Molnar <gabormolnar92@gmail.com>
2014-09-18 19:45:31 +0200

    Navbar

--- app/views/_ik/header.blade.php
+++ app/views/_ik/header.blade.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">DE-IK</a>
+          {{HTML::link('ik','Debreceni Egyetem Informatikai Kar',array('class'=>'navbar-brand'))}}
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
