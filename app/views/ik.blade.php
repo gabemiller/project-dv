d43b021a0274a9426deaef6402840e09d30dbd9c
Gabor Molnar <gabormolnar92@gmail.com>
2014-09-18 20:03:43 +0200

    remove target _blank

--- app/views/ik.blade.php
+++ app/views/ik.blade.php
@@ -15,9 +15,9 @@
 
 <p>{{HTML::link('http://www.inf.unideb.hu/~csernochmaria/bev_info/','Tantárgy honlapja',array('target'=>'_blank'))}}</p>
 
-<p>Számrendszerek gyakorló {{HTML::link('ik/szamrendszerek','feladatsor',array('target'=>'_blank'))}} megoldással.</p>
-<p>Számábrázolás gyakorló {{HTML::link('ik/szamabrazolas','feladatsor',array('target'=>'_blank'))}} megoldással + segédanyag.</p>
-<p>Aritmetikai feladatok gyakorló {{HTML::link('ik/aritmetika','feladatsor',array('target'=>'_blank'))}} megoldással.</p>
+<p>Számrendszerek gyakorló {{HTML::link('ik/szamrendszerek','feladatsor',array())}} megoldással.</p>
+<p>Számábrázolás gyakorló {{HTML::link('ik/szamabrazolas','feladatsor',array())}} megoldással + segédanyag.</p>
+<p>Aritmetikai feladatok gyakorló {{HTML::link('ik/aritmetika','feladatsor',array())}} megoldással.</p>
 
 <p>C programozáshoz egy kis {{HTML::link('http://mezeic.uw.hu/','segédanyag',array('target'=>'_blank'))}}</p>
 <p class="alert alert-danger">CodeBlocks <a href="http://www.codeblocks.org/downloads/26" target="_blank">letöltés</a> és itt ezt kell letölteni: <strong>codeblocks-12.11mingw-setup.exe</strong>. Fontos, hogy a mingw benne legyen, mert az a compiler.</p>
